5a7a1187af970db20d37f287b839a817cfb39cda
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-05-07 14:43:54 +1200

    fdasfdsa

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# score
+
+score = 0
+
 # Ask The User Their Name And Store It
 
 name = input("What is Your Name?")
@@ -14,6 +18,15 @@ answer = input("What is 1 + 1? ")
 # Check The User's Answer and Give Feedback
 if answer == "2":
     print("Nice, you got it correct")
+    score +=1
+elif answer == "":
+    print("Please type in an answer")
+    answer = input("What is 1 + 1?")
+    if answer == "2":
+        print("Nice, you got it correct")
+        score +=1
+    else: 
+        print("Sadly, this is incorrect. The answer is 2")
 else:
     print("Sadly, this is incorrect. The answer is 2")
 
@@ -26,8 +39,17 @@ answerone = input("What is 12 x 12? ")
 
 if answerone == "144":
     print("Nice, you got it correct")
+    score +=1
+elif answerone == "":
+    print("Please type in an answer")
+    answerone = input("What is 12 x 12?")
+    if answerone == "144":
+        score +=1
+        print("Nice, you got it correct. 12 x 12 is equal to 144")
+    else:
+        print("Incorroect, 12x12 is equal to 144")
 else:
-    print("Inccorect, 12 x 12 is equal to 144")
+    print("Incorrect, 12 x 12 is equal to 144")
 
 
 # Question 3
@@ -39,10 +61,20 @@ answertwo = input("Simplify: 2x+2k-4k: ")
 
 if answertwo == "2x-2k":
     print("Nice, that is correct.")
+    score +=1
+elif answertwo == "":
+    print("Please type in an answer")
+    answertwo = input("Simplify: 2x + 2k - 4k:")
+    if answertwo == "2x-2k":
+        print("Nice, you got it correct")
+        score +=1
+    else:
+        print("Nice try, but incorect. The answer is 2x-2k. Remember to add like terms together.")
 else:
     print("Nice try, but incorect. The answer is 2x-2k. Remember to add like terms together.")
 
 #End The Quiz
 
 print("Thank you for playing my quiz")
+print("Your final score is", score)
 print("Have a great day",name)
