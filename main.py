3438f7ed606c067453677e8e16b7eb5f910130ff
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-05-14 15:00:45 +1200

    finished version 4, starting on version 5

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import random
+
+GOOD_COMMENT = ["Nice job", "You're smart", "Cool", "Sigma"]
+BAD_COMMENT = ["Dumb", "Non-sigma", "Cmon", "Lock in"]
+
 # play
 play = "yes"
 
@@ -19,7 +24,7 @@ while play == "yes":
     # Asks The User How Many Attempts They Want
     while True:
         try:
-            attempt = int(input("How many attempts would you like per question? 1, 2, 3 or 4 attempts?"))
+            attempt = int(input("How many attempts would you like for the quiz? 1, 2, 3 or 4 attempts?"))
             break
         except:
             print("That is not an integer")
@@ -39,6 +44,7 @@ while play == "yes":
         # Check The User's Answer and Give Feedback
         if answer == "2" or answer == b or answer == "b":
             print("Nice, you got it correct")
+            print(random.choice(GOOD_COMMENT))
             score += 1
             break
         elif answer == "":
@@ -46,84 +52,109 @@ while play == "yes":
             answer = input("What is 1 + 1?")
             if answer == "2".lower():
                 print("Nice, you got it correct")
+                print(random.choice(GOOD_COMMENT))
                 score += 1
             else:
-                print("Sadly, this is incorrect. The answer is 2, or B")
+                print("Sadly, this is incorrect.")
+                print(random.choice(BAD_COMMENT))
+                question_tries -= 1
+                print("You have {} attempt(s) left".format(question_tries))
         elif answer != "a".lower() and answer != "b".lower() and answer != "c".lower() and answer != "d".lower() and answer != "4" and answer != "3" and answer != "2" and answer != "1":
             print("That wasn't an option")
+            print(random.choice(BAD_COMMENT))
         else:
             print("Sadly, this is incorrect.")
+            print(random.choice(BAD_COMMENT))
             question_tries -= 1
-        if question_tries == 0:
-            ("You have no attempts left.")
+            print("You have {} attempt(s) left".format(question_tries))
+
 
     # Next Question
+    while question_tries > 0:
+        print("Here is an even harder question:")
+        questionone = ("What is 12 x 12? ").lower()
+        a1 = "144"
+        b1 = "1212"
+        c1 = "24"
+        d1 = "120"
 
-    print("Here is an even harder question:")
-    questionone = ("What is 12 x 12? ").lower()
-    a1 = "144"
-    b1 = "1212"
-    c1 = "24"
-    d1 = "120"
+        answerone = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(questionone, a1, b1, c1, d1)).lower()
 
-    answerone = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(questionone, a1, b1, c1, d1)).lower()
+        # Check The User's Answer and Give Feedback
 
-    # Check The User's Answer and Give Feedback
-
-    if answerone == "144".lower() or answerone == "A".lower() or answerone == "a":
-        print("Nice, you got it correct")
-        score += 1
-    elif answerone == "":
-        print("Please type in an answer")
-        answerone = input("What is 12 x 12?")
-        if answerone == "144".lower():
+        if answerone == "144".lower() or answerone == "A".lower() or answerone == "a":
+            print("Nice, you got it correct")
+            print(random.choice(GOOD_COMMENT))
             score += 1
-            print("Nice, you got it correct. 12 x 12 is equal to 144")
+            break
+        elif answerone == "":
+            print("Please type in an answer")
+            answerone = input("What is 12 x 12?")
+            if answerone == "144".lower():
+                score += 1
+                print("Nice, you got it correct. 12 x 12 is equal to 144")
+                print(random.choice(GOOD_COMMENT))
+            else:
+                print("Incorrect")
+                print(random.choice(BAD_COMMENT))
+                question_tries -= 1
+                print("You have {} attempt(s) left".format(question_tries))
+        elif answerone != "a".lower() and answerone != "b".lower() and answerone != "c".lower() and answerone != "d".lower() and answerone != "144" and answerone != "1212" and answerone != "24" and answerone != "120":
+            print("That wasn't an option")
+            question_tries -= 1
+            print("You have {} attempt(s) left".format(question_tries))
         else:
-            print("Incorroect, 12x12 is equal to 144")
-    elif answerone == "I don't know" and answerone == "IDK".lower() and answerone == "?":
-        print("Not sure?")
-        print("The answer is 144")
-    elif answerone != "a".lower() and answerone != "b".lower() and answerone != "c".lower() and answerone != "d".lower() and answerone != "144" and answerone != "1212" and answerone != "24" and answerone != "120":
-        print("That wasn't an option")
-    else:
-        print("Incorrect, 12 x 12 is equal to 144")
+            print("Incorrect.")
+            print(random.choice(BAD_COMMENT))
+            question_tries -= 1
+            print("You have {} attempt(s) left".format(question_tries))
 
     # Question 3
+    while question_tries > 0:
+        print("Here is an even harder question about like terms:")
+        questiontwo = ("Simplify: 2x+2k-4k: ").lower()
+        a2 = "-2k-2x"
+        b2 = "2a+15"
+        c2 = "2x-2k"
+        d2 = "2x+2k"
 
-    print("Here is an even harder question about like terms:")
-    questiontwo = ("Simplify: 2x+2k-4k: ").lower()
-    a2 = "-2k-2x"
-    b2 = "2a+15"
-    c2 = "2x-2k"
-    d2 = "2x+2k"
+        answertwo = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(questiontwo, a2, b2, c2, d2)).lower()
 
-    answertwo = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(questiontwo, a2, b2, c2, d2)).lower()
-
-    # Check The User's Answer and Give Feedback
-
-    if answertwo == c.lower() or answertwo == "c".lower():
-        print("Nice, that is correct.")
-        score += 1
-    elif answertwo == "":
-        print("Please type in an answer")
-        answertwo = input("Simplify: 2x + 2k - 4k:")
-        if answertwo == "2x-2k":
-            print("Nice, you got it correct")
+        # Check The User's Answer and Give Feedback
+        if answertwo == c.lower() or answertwo == "c".lower():
+            print("Nice, that is correct.")
+            print(random.choice(GOOD_COMMENT))
             score += 1
+            break
+        elif answertwo == "":
+            print("Please type in an answer")
+            answertwo = input("Simplify: 2x + 2k - 4k:")
+            if answertwo == "2x-2k":
+                print("Nice, you got it correct")
+                print(random.choice(GOOD_COMMENT))
+                score += 1
+            else:
+                print(
+                    "Nice try, but incorect. Remember to add like terms together."
+                )
+                print(random.choice(BAD_COMMENT))
+                question_tries -= 1
+                print("You have {} attempt(s) left".format(question_tries))
+        elif answertwo != "a".lower() and answertwo != "b".lower() and answertwo != "c".lower() and answertwo != "d".lower() and answertwo != "-2k-2x" and answertwo != "2a+15" and answertwo != "2x-2k" and answertwo != "2x+2k" or answertwo == "3":
+            print("That wasn't an option")
         else:
             print(
-                "Nice try, but incorect. The answer is 2x-2k. Remember to add like terms together."
+                "Nice try, but incorect. Remember to add like terms together."
             )
-    else:
-        print(
-            "Nice try, but incorect. The answer is 2x-2k. Remember to add like terms together."
-        )
-    
+            print(random.choice(BAD_COMMENT))
+            question_tries -= 1
+            print("You have {} attempt(s) left".format(question_tries))
+    if question_tries == 0:
+        print("You have used up all of your attempts.")
+    print("Your final score is {}".format(score))
     play = input("Would you like to play again?")
 
 #End The Quiz
 
 print("Thank you for playing my quiz")
-print("Your final score is {}".format(score))
 print("Have a great day {}".format(name))
