e2da8b53788f14e0586a9c6a654cc45d2d1052d1
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-05-16 10:27:45 +1200

    dsafdsafsdafsdaf

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import random
 
 GOOD_COMMENT = ["Nice job", "You're smart", "Cool", "Sigma"]
-BAD_COMMENT = ["Dumb", "Non-sigma", "Cmon", "Lock in"]
+
+BAD_COMMENT = ["Dumb", "Keep on going", "Cmon", "Lock in"]
+
+QUIZ_QUESTIONS = ["What is 1 + 1? ",
+                  "What is 12 x 12? ",
+                  "Simplify 2x+2k-4k:"]
+
+QUIZ_OPTIONS = [["1" , "2" , "3" , "4"],
+                ["144","1212","24","120"],
+                ["-2k-2x", "2a+15", "2x-2k", "2x+2k"]]
+
+QUIZ_LIST_OPTION = ["A","B","C","D"]
+
+QUIZ_ANSWERS = [1,0,2]
+
+QUESTION_FORMAT = {"{}\nA.{}\nB.{}\nC.{}\nD.{}\n"}
+
+    # Ask The User Their Name And Store It
+
+name = input("What is Your Name?")
 
 # play
 play = "yes"
 
-while play == "yes":
+while play == "yes".lower():
 
     # score
 
     score = 0
-
-    # Ask The User Their Name And Store It
-
-    name = input("What is Your Name?")
 
     # Greet The User And Introduce The Quiz
 
@@ -34,15 +49,12 @@ while play == "yes":
     question_tries = attempt
 
     while question_tries > 0:
-        question = ("What is 1 + 1? ")
-        a = "1"
-        b = "2"
-        c = "3"
-        d = "4"
-        answer = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(question, a, b, c, d)).lower()
+
+        answer = input("{}\nA.{}\nB.{}\nC.{}\nD.{}\n".format(QUIZ_QUESTIONS[0], QUIZ_OPTIONS[0][0],
+                                              QUIZ_OPTIONS[0][1],QUIZ_OPTIONS[0][2],QUIZ_OPTIONS[0][3])).lower()
 
         # Check The User's Answer and Give Feedback
-        if answer == "2" or answer == b or answer == "b":
+        if answer == QUIZ_OPTIONS[0][QUIZ_ANSWERS[0]] or answer == QUIZ_LIST_OPTION[QUIZ_ANSWERS[0]]:
             print("Nice, you got it correct")
             print(random.choice(GOOD_COMMENT))
             score += 1
@@ -59,15 +71,18 @@ while play == "yes":
                 print(random.choice(BAD_COMMENT))
                 question_tries -= 1
                 print("You have {} attempt(s) left".format(question_tries))
-        elif answer != "a".lower() and answer != "b".lower() and answer != "c".lower() and answer != "d".lower() and answer != "4" and answer != "3" and answer != "2" and answer != "1":
-            print("That wasn't an option")
-            print(random.choice(BAD_COMMENT))
-        else:
+        elif answer in QUIZ_LIST_OPTION or answer in QUIZ_OPTIONS[0]:
             print("Sadly, this is incorrect.")
             print(random.choice(BAD_COMMENT))
             question_tries -= 1
             print("You have {} attempt(s) left".format(question_tries))
 
+        else: 
+            print("That wasn't an option")
+            print(random.choice(BAD_COMMENT))
+            question_tries -= 1
+            print("You have {} attempt(s) left".format(question_tries))
+            
 
     # Next Question
     while question_tries > 0:
@@ -121,7 +136,7 @@ while play == "yes":
         answertwo = input("{}\nA.{} \nB.{} \nC.{} \nD.{}\nAnswer:".format(questiontwo, a2, b2, c2, d2)).lower()
 
         # Check The User's Answer and Give Feedback
-        if answertwo == c.lower() or answertwo == "c".lower():
+        if answertwo == c2.lower() or answertwo == "c".lower():
             print("Nice, that is correct.")
             print(random.choice(GOOD_COMMENT))
             score += 1
@@ -152,7 +167,7 @@ while play == "yes":
     if question_tries == 0:
         print("You have used up all of your attempts.")
     print("Your final score is {}".format(score))
-    play = input("Would you like to play again?")
+    play = input("Would you like to play again?").lower()
 
 #End The Quiz
 
