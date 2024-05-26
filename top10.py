6a933d3694e496c5147c3bf5719816416788af57
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-05-27 11:58:45 +1200

    fsdafdsa

--- top10.py
+++ top10.py
@@ -1,3 +1,6 @@
+# - Lists - #
+TOP_TEN = ["France", "Spain", "United States", "Italy", "Turkey", "Mexico", "United Kingdom", "Germany","Greece","Austria"]
+
 #------ FUNCTIONS ------#
 
 #- Introduces the quiz -#
@@ -8,9 +11,41 @@ def intro():
   name = input("What is your name?")
 
   print("Welcome to this quiz, {}".format(name))
-  print("This quiz is about guessing the top 10 of a topic")
+  print("This quiz is about guessing the top 10 most visited countries in the world")
 
+def getLives():
 
+  while True:
+    try:
+      attempts = input("How many chances would you like?")
+      attempts = int(attempts)
+      if attempts > 0:
+        return attempts
+      else:
+        print("Please type in a positive number")
+    except:
+      print("That is not a number")
+
+def inList(question,TOP_TEN):
+  if question in TOP_TEN:
+    return True
+  else:
+    return False
 #------ MAIN CODE ------#
 
+score = 0 
+
 intro()
+lives = getLives()
+
+while lives > 0:
+  question = input("\nName the top 10 most visited countried in the world:\n").lower()
+
+  if inList(question, TOP_TEN) == True:
+    score =+ 5
+    print("Nice, you're correct")
+  else:
+    lives -= 1
+    print("That is incorrect.\nYou have {} lives left".format(lives))
+
+print("You're out of lives. Game Over")
