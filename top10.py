abe1423f6b23dbcf9fc1cd78e55c32cb7bec60f8
bnuaWSCW <nuab@wsc.school.nz>
2024-05-28 16:29:50 +1200

    finished v6

--- top10.py
+++ top10.py
@@ -1,5 +1,6 @@
 # - Lists - #
-TOP_TEN = ["France", "Spain", "United States", "Italy", "Turkey", "Mexico", "United Kingdom", "Germany","Greece","Austria"]
+guesses = []
+TOP_TEN = ["France", "Spain", "United States", "Italy", "Turkey", "Mexico", "United Kingdom", "Germany", "Greece", "Austria"]
 
 #------ FUNCTIONS ------#
 
@@ -8,7 +9,7 @@ TOP_TEN = ["France", "Spain", "United States", "Italy", "Turkey", "Mexico", "Uni
 
 def intro():
 
-  name = input("What is your name?")
+  name = input("Type In Your Username: ")
 
   print("Welcome to this quiz, {}".format(name))
   print("This quiz is about guessing the top 10 most visited countries in the world")
@@ -16,36 +17,65 @@ def intro():
 def getLives():
 
   while True:
-    try:
-      attempts = input("How many chances would you like?")
-      attempts = int(attempts)
-      if attempts > 0:
-        return attempts
-      else:
-        print("Please type in a positive number")
-    except:
-      print("That is not a number")
+      attempts = input("How many chances would you like?\n")
+      try:
+        attempts = int(attempts)
+        if attempts > 0:
+          return attempts
+        else:
+          print("Please type in a positive number")
+      except:
+        print("That is not a number")
 
-def inList(question,TOP_TEN):
-  if question in TOP_TEN:
+def inList(answer, list):
+  if answer in list:
     return True
   else:
     return False
+  
+
 #------ MAIN CODE ------#
+play = "Yes".lower()
 
-score = 0 
+while play == "yes".lower():
 
-intro()
-lives = getLives()
+  intro()
+  lives = getLives()
+  score = 0 
 
-while lives > 0:
-  question = input("\nName the top 10 most visited countried in the world:\n").lower()
+  while lives > 0 and score < 10:
+    answer = input("\nName the top 10 most visited countried in the world:\n").title()
 
-  if inList(question, TOP_TEN) == True:
-    score =+ 5
-    print("Nice, you're correct")
-  else:
-    lives -= 1
-    print("That is incorrect.\nYou have {} lives left".format(lives))
+    inList(answer, TOP_TEN)
 
-print("You're out of lives. Game Over")
+    if inList(answer, TOP_TEN) == True:
+      if inList(answer, guesses):
+        print("You have already guessed that")
+      else:
+        score += 1
+        print("Nice, you're correct")
+        guesses.append(answer)
+        print("You have guessed {} country(s) with {} live(s) left. Right now, your score is {} out of 10".format(len(guesses), lives, score))
+        if  int(len(guesses)) > 0:
+          print("\nYou have guessed the following CORRECT countries:")
+          print(guesses)
+        elif int(len(guesses)) == 10:
+          break
+    else:
+      lives -= 1
+      print("That is incorrect.\nYou have {} lives left with a score of {}.".format(lives, score))
+      if int(len(guesses)) > 0:
+        print("You have guessed the following countries:")
+        print(guesses)
+
+
+  if lives == 0:
+    print("You're out of lives.\nYour final score was {}\nGame Over.".format(score))
+    play = input("\nWould you like to play again?").lower
+
+  if score == 10 or int(len(guesses)) == 10:
+    print("\nCongrats, you have guessed all ten of the most visted countries in the world")
+    print("Thank you for playing")
+    play != "Yes".lower()
+    
+
